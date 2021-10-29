bec6bce9e81f42c6cba5a9cc2f15d72be43a6005
zadammac <zadammac@kenshosec.com>
2021-10-29 13:41:06 -0300

    See changelog - v 0.0.4

---
+++ lib/menus/main_game.h
@@ -0,0 +1,35 @@
+/*
+ * main_game.h
+ *
+ *  Created on: Oct. 26, 2021
+ *      Author: patches
+ */
+
+#ifndef LIB_MENUS_MAIN_GAME_H_
+#define LIB_MENUS_MAIN_GAME_H_
+
+#pragma PERSISTENT(MG_count_menu_icons)
+const int MG_count_menu_icons = 6;
+
+#pragma PERSISTENT(MG_menu_icons)
+const char MG_menu_icons[6] = {
+                               0x07,
+                               0x08,
+                               0x09,
+                               0x0A,
+                               0x0B,
+                               0x0C
+};
+
+#pragma PERSISTENT(MG_menu_scene_addresses)
+const int MG_menu_scene_addresses[6] = {
+                                      SCENEADDR_proof_text,
+                                      SCENEADDR_proof_text,
+                                      SCENEADDR_proof_text,
+                                      SCENEADDR_proof_text,
+                                      SCENEADDR_demo_mode,
+                                      SCENEADDR_calendar_menu
+};
+
+
+#endif /* LIB_MENUS_MAIN_GAME_H_ */
