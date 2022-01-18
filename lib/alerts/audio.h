19a4eabec190656bda573c2244aaa3dc7fbc0031
zadammac <zadammac@kenshosec.com>
2022-01-18 19:31:08 -0400

    Add the audio alert primitive for pulses of various lengths

---
+++ lib/alerts/audio.h
@@ -0,0 +1,18 @@
+/*
+ * audio.h
+ *
+ *  Created on: Jan. 18, 2022
+ *      Author: patches
+ */
+
+
+
+#ifndef LIB_ALERTS_AUDIO_H_
+#define LIB_ALERTS_AUDIO_H_
+
+#define LONG_PULSE 0x4444
+#define SHORT_PULSE 0x4444
+
+void AUDIO_pulse(int pulse_length);
+
+#endif /* LIB_ALERTS_AUDIO_H_ */
