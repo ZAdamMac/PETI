10379b8de8ce253c370f703da59864de614e2884
zadammac <zadammac@arcanalabs.ca>
2025-03-05 20:48:44 -0400

    Start an icon defintion header for future magic number removals

---
+++ src/lib/display/icons.h
@@ -0,0 +1,17 @@
+/*
+ * font.h
+ *
+ *  Created on: 2020-11-28; Updated 2024-02-06
+ *      Author: github.com/zadammac
+ */
+
+#ifndef ICONS_H_
+#define ICONS_H_
+
+#define ICON_POOP_A 'I'
+#define ICON_POOP_B 'Y'
+#define ICON_POOP_FONT FONT_ADDR_1
+
+
+
+#endif /* ICONS_H_ */
